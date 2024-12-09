18059ef9df43578c25c63a7761e1563ced0e08f6
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-12-09 18:50:02 +0300

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, request.GET.get('per_page', 3))
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post_list.html', {'page_obj': page_obj})
